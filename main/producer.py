6e7b0a55b5e1909ae53051db5069c912c105f37f
Bimo Syahputro <bimosyahputro88@gmail.com>
2021-08-20 14:35:22 +0700

    first commit

---
+++ main/producer.py
@@ -0,0 +1,11 @@
+import pika, json
+
+params = pika.URLParameters('amqps://kdpilrrn:8F9XWgk9wvsjSfc4riOTCZ7AvZ5ytqqz@elk.rmq2.cloudamqp.com/kdpilrrn')
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='admin', body=json.dumps(body), properties=properties)
